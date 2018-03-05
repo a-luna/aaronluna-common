3a2f7336a14c1da031e7d7fb095b27beec4d53cd
Aaron Luna <aaronluna@gmail.com>
2018-03-05 05:49:22 -0800

    Added non-ascii character encoding class, modified progress bar class with new animation strings.

--- AaronLuna.Common/Console/ConsoleProgressBar.cs
+++ AaronLuna.Common/Console/ConsoleProgressBar.cs
@@ -1,4 +1,7 @@
-﻿namespace AaronLuna.Common.Console
+﻿using System.Linq;
+using AaronLuna.Common.Numeric;
+
+namespace AaronLuna.Common.Console
 {
     using System;
     using System.Text;
@@ -6,6 +9,19 @@
 
     public class ConsoleProgressBar : IDisposable, IProgress<double>
     {
+        public static string RotatingArrowAnimation = "\u2190\u2196\u2191\u2197\u2192\u2198\u2193\u2199";
+        public static string GrowingBarAnimation = "\u2581\u2582\u2583\u2584\u2585\u2586\u2587\u2588\u2587\u2586\u2585\u2584\u2583\u2581";
+        public static string BrailleAnimation1 = "\u28fe\u28fd\u28fb\u28bf\u287f\u28df\u28ef\u28f7";
+        public static string BrailleAnimation2 = "\u2801\u2802\u2804\u2840\u2880\u2820\u2810\u2808";
+        public static string EyeAnimation = "\u25e1\u25e1\u2299\u2299\u25e0\u25e0";
+        public static string SemicircleAnimation = "\u25d0\u25d3\u25d1\u25d2";
+        public static string RotatingTriangleAnimation = "\u25e2\u25e3\u25e4\u25e5";
+        public static string RotatingSquareAnimation = "\u2596\u2598\u259d\u2597";
+        public static string RotatingPipeAnimation = "\u2524\u2518\u2534\u2514\u251c\u250c\u252c\u2510";
+        public static string BouncingBallAnimation = ".oO\u00b0Oo.";
+        public static string ExplodingAnimation = ".oO@*";
+        public static string DefaultSpinnerAnimation = @"|/-\-";
+
         readonly TimeSpan _animationInterval = TimeSpan.FromSeconds(1.0 / 8);
 
         Timer _timer;
@@ -19,19 +35,21 @@
             Initialize();
 
             NumberOfBlocks = 10;
-            StartBracketChar = '[';
-            EndBracketChar = ']';
-            CompletedBlockChar = '#';
-            UncompletedBlockChar = '-';
-            AnimationSequence = @"|/-\-";
+            StartBracket = "[";
+            EndBracket = "]";
+            CompletedBlock = "#";
+            UncompletedBlock = "-";
+            AnimationSequence = DefaultSpinnerAnimation;
         }
 
         public int NumberOfBlocks { get; set; }
-        public char StartBracketChar { get; set; }
-        public char EndBracketChar { get; set; }
-        public char CompletedBlockChar { get; set; }
-        public char UncompletedBlockChar { get; set; }
-        public string AnimationSequence { get; set; }
+        public string StartBracket { get; set; }
+        public string EndBracket { get; set; }
+        public string CompletedBlock { get; set; }
+        public string UncompletedBlock { get; set; }
+        public string AnimationSequence { get; set; }        
+        public long BytesReceived { get; set; }
+        public long FileSizeInBytes { get; set; }
 
         public void Report(double value)
         {
@@ -67,12 +85,24 @@
 
         private string GetProgressBarText(double currentProgress)
         {
-            var numBlocksCompleted = (int)(currentProgress * NumberOfBlocks);            
-            var completedBlocks = new string(CompletedBlockChar, numBlocksCompleted);
-            var uncompletedBlocks = new string(UncompletedBlockChar, NumberOfBlocks - numBlocksCompleted);
+            var numBlocksCompleted = (int)(currentProgress * NumberOfBlocks);
 
-            var progressBar = $"{StartBracketChar}{completedBlocks}{uncompletedBlocks}{EndBracketChar}";
+            var completedBlocks = string.Empty;
+            foreach (var i in Enumerable.Range(0, numBlocksCompleted))
+            {
+                completedBlocks += CompletedBlock;
+            }
+
+            var uncompletedBlocks = string.Empty;
+            foreach (var i in Enumerable.Range(0, NumberOfBlocks - numBlocksCompleted))
+            {
+                uncompletedBlocks += UncompletedBlock;
+            }            
+
+            var progressBar = $"{StartBracket}{completedBlocks}{uncompletedBlocks}{EndBracket}";
             var percent = (int)(currentProgress * 100);
+            var bytesReceived = BytesReceived.ConvertBytesForDisplay();
+            var fileSizeInBytes = FileSizeInBytes.ConvertBytesForDisplay();
             var animation = AnimationSequence[_animationIndex++ % AnimationSequence.Length];
 
             if (currentProgress is 1)
@@ -80,7 +110,7 @@
                 animation = ' ';
             }
 
-            return $"{progressBar} {percent}% {animation}";            
+            return $"{progressBar} {percent}% {bytesReceived} of {fileSizeInBytes}";            
         }
 
         private void UpdateText(string text)
