a8355f7c08212e4a3e53d173094b1cbbda1906f8
Aaron Luna <aaronluna@gmail.com>
2018-03-05 19:22:53 -0800

    Added public static strings to ConsoleProgressBar for spinner/activity animations. Added new class NonAsciiCharacterEncoder which helped create the strings or escaped unicode chars which create the animations.

---
+++ AaronLuna.Common/String/NonAsciiCharacterEncoder.cs
@@ -0,0 +1,37 @@
+﻿namespace AaronLuna.Common.String
+{
+    using System.Globalization;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public static class NonAsciiCharacterEncoder
+    {
+        public static string EncodeNonAsciiCharacters(string value)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in value)
+            {
+                if (c > 127)
+                {
+                    // This character is too big for ASCII
+                    string encodedValue = "\\u" + ((int) c).ToString("x4");
+                    sb.Append(encodedValue);
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static string DecodeEncodedNonAsciiCharacters(string value)
+        {
+            return Regex.Replace(
+                value,
+                @"\\u(?<Value>[a-zA-Z0-9]{4})",
+                m => { return ((char) int.Parse(m.Groups["Value"].Value, NumberStyles.HexNumber)).ToString(); });
+        }
+    }
+}
