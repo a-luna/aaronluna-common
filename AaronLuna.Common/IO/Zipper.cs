17afc9f6eed6d8a296deb15603d61089c8c78711
Aaron Luna <aaronluna@gmail.com>
2018-05-17 09:59:46 -0700

    Added Zipper class and associate test cases/test files

---
+++ AaronLuna.Common/IO/Zipper.cs
@@ -0,0 +1,50 @@
+﻿namespace AaronLuna.Common.IO
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.IO.Compression;
+
+    using Result;
+
+    public static class Zipper
+    {
+        public static Result CreateArchive(List<FileInfo> filesToCompress, string outputFile, bool deleteIfAlreadyExists=false)
+        {
+            var outputFolder = Path.GetDirectoryName(outputFile);
+            Directory.CreateDirectory(outputFolder);
+
+            if (File.Exists(outputFile))
+            {
+                if (!deleteIfAlreadyExists)
+                {
+                    return Result.Fail($"A file named \"{Path.GetFileName(outputFile)}\" already exists at {outputFolder}.");
+                }
+
+                File.Delete(outputFile);
+            }
+
+            if (filesToCompress.Count <= 0)
+            {
+                return Result.Fail("File list contains zero files.");
+            }
+
+            try
+            {
+                using (var fs = new FileStream(outputFile, FileMode.Create))
+                using (var arch = new ZipArchive(fs, ZipArchiveMode.Create))
+                {
+                    foreach (var file in filesToCompress)
+                    {
+                        arch.CreateEntryFromFile(file.FullName, file.Name);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                return Result.Fail($"{ex.Message} {ex.GetType()}");
+            }
+
+            return Result.Ok();
+        }
+    }
+}
---
+++ AaronLuna.CommonTest/IO/ZipperTestFixture.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using AaronLuna.Common.IO;
+
+namespace AaronLuna.CommonTest.IO
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ZipperTestFixture
+    {
+
+        [TestMethod]
+        public void VerifyCreateArchive()
+        {
+            var currentPath = Directory.GetCurrentDirectory();
+            var index = currentPath.IndexOf("bin", StringComparison.Ordinal);
+            var testFilesFolder = $"{currentPath.Remove(index - 1)}{Path.DirectorySeparatorChar}IO{Path.DirectorySeparatorChar}ZipperTestFixture{Path.DirectorySeparatorChar}";
+            var outputFile = Path.Combine(testFilesFolder, "test.zip");
+
+            var filesToCompress = new List<FileInfo>
+            {
+                new FileInfo(Path.Combine(testFilesFolder, "file1.txt")),
+                new FileInfo(Path.Combine(testFilesFolder, "file2.txt")),
+                new FileInfo(Path.Combine(testFilesFolder, "file3.txt")),
+                new FileInfo(Path.Combine(testFilesFolder, "file4.txt")),
+                new FileInfo(Path.Combine(testFilesFolder, "file5.txt"))
+            };
+
+            var result = Zipper.CreateArchive(filesToCompress, outputFile);
+            if (result.Failure)
+            {
+                Assert.Fail(result.Error);
+            }
+
+            Assert.IsTrue(File.Exists(outputFile));
+            File.Delete(outputFile);
+        }
+    }
+}
