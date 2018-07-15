3ea5a1b89f9497143589242ef33bb438eaf4d077
Aaron Luna <aaronluna@gmail.com>
2018-07-15 06:46:31 -0700

    Added AsyncLock class

---
+++ AaronLuna.Common/Threading/AsyncLock.cs
@@ -0,0 +1,22 @@
+﻿namespace AaronLuna.Common.Threading
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class AsyncLock : IDisposable
+    {
+        private readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1, 1);
+
+        public async Task<AsyncLock> LockAsync()
+        {
+            await _semaphoreSlim.WaitAsync();
+            return this;
+        }
+
+        public void Dispose()
+        {
+            _semaphoreSlim.Release();
+        }
+    }
+}
